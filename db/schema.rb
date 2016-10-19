3976ec006fb0bd07b9065a82c8716578de2a572e
lucas nunes de sousa <lucasnshunter@hotmail.com>
2016-10-19 13:27:20 +0000

    representantes com layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011132003) do
+ActiveRecord::Schema.define(version: 20161011133115) do
 
   create_table "representantes", force: :cascade do |t|
     t.string   "responsavel"
@@ -47,5 +47,13 @@ ActiveRecord::Schema.define(version: 20161011132003) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "zonas", force: :cascade do |t|
+    t.string   "cidade"
+    t.string   "estado"
+    t.integer  "representante_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end
